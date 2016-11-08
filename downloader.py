3bf6e0fa47a5b51ad90780ab82cb5e8de4f5d76c
Stuart Jackson <stuartrj1@gmail.com>
2016-11-08 11:13:20 +0200

    Update downloader.py
    Fixed issue in showing icon when freezing
    Added check for ffmpeg and ffprobe for mp3 support
    Moved binary files to user dir

--- downloader.py
+++ downloader.py
@@ -10,17 +10,49 @@ from PySide.QtCore import QProcess
 
 from ui_downloader import Ui_MainWindow
 
+__version__ = '0.0.2'
+
+
 class YouTube:
     def __init__(self):
-        self.version_file = os.path.join(os.getcwd(),'youtube-dl.version')
+        self.homedir = os.path.join(os.path.expanduser("~"), '.ytd')
+        if not os.path.exists(self.homedir):
+            os.makedirs(self.homedir)
+        self.version_file = os.path.join(self.homedir, 'youtube-dl.version')
 
-    def download_file(self,url):
-        f = urlopen(url)
-        print "downloading " + url
-        # Open our local file for writing
-        with open(os.path.basename(url), "wb") as local_file:
-            local_file.write(f.read())
-        open(self.version_file,'w').write(url)
+    def download_file(self, url, ytd=None):
+        u = urlopen(url)
+        meta = u.info()
+        file_size = int(meta.getheaders('Content-Length')[0])
+        print file_size
+
+        f = open(os.path.join(self.homedir, os.path.basename(url)), 'wb')
+
+        downloaded_bytes = 0
+        block_size = 1024 * 512
+        while True:
+            buffer = u.read(block_size)
+            if not buffer:
+                break
+
+            f.write(buffer)
+            downloaded_bytes += block_size
+            print downloaded_bytes
+
+        if ytd:
+            open(self.version_file, 'w').write(url)
+
+    def check_ffmpeg(self):
+        if not os.path.exists(os.path.join(self.homedir, 'ffmpeg.exe')):
+            return 'http://ffmpeg.jagnet.biz/ffmpeg.exe'
+        else:
+            return None
+
+    def check_ffprobe(self):
+        if not os.path.exists(os.path.join(self.homedir, 'ffprobe.exe')):
+            return 'http://ffmpeg.jagnet.biz/ffprobe.exe'
+        else:
+            return None
 
     def get_exe_url(self):
         try:
@@ -34,7 +66,7 @@ class YouTube:
 
     def check_version(self):
         if not os.path.exists(self.version_file):
-            open(self.version_file,'w').close()
+            open(self.version_file, 'w').close()
         curr_version = open(self.version_file, 'r').read()
         remote_version = self.get_exe_url()
         if curr_version != remote_version and remote_version is not None:
@@ -61,16 +93,20 @@ class YouTube:
             except URLError, e:
                 title = 'Unable to search ({0})'.format(e.reason)
                 video_link = ''
-            return {'title':title, 'video_link':video_link}
+            return {'title': title, 'video_link': video_link}
 
     def video_download(self, video_link, outputdir):
-        command = '''youtube-dl --output "{0}/%(title)s.%(ext)s" "{1}"'''.format(outputdir, video_link)
+        command = '''{2}\\youtube-dl --output "{0}/%(title)s.%(ext)s" "{1}"'''.format(outputdir, video_link, self.homedir)
         print command
         os.system(command)
 
     def audio_download(self, video_link, outputdir):
-        command = '''youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 --output "{0}/%(title)s.%(ext)s" "{1}"'''.format(outputdir, video_link)
+        command = '''{2}\\youtube-dl --extract-audio \
+--audio-format mp3 --audio-quality 0 --output "{0}/%(title)s.%(ext)s" "{1}"'''.format(outputdir,
+                                                                                      video_link,
+                                                                                      self.homedir)
         os.system(command)
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -97,30 +133,33 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.yt = YouTube()
         self.youtube_dl_version_check()
-
-        #some test data
-        #self.txtbxUrl.setText('http://www.youtube.com/watch?v=AgFeZr5ptV8')
-        #self.rbUrl.toggle()
-        #self.source_rb_changed()
-        #self.lblOutputDir.setText(os.getcwd())
+        self.ffmpeg_check()
+        self.ffprobe_check()
 
     def setup_form(self):
         self.btnSearch.setEnabled(False)
         self.txtbxUrl.setEnabled(False)
         self.txtbxSearch.setEnabled(False)
         self.rbDlVideo.toggle()
-        self.txtOutputText.setVisible(False)
+        self.btnDownload.setEnabled(False)
+        self.check_dl_ready()
+
+    def check_dl_ready(self):
+        if self.lblOutputDir.text() != '' and self.txtbxUrl.text() != '':
+            self.btnDownload.setEnabled(True)
+        else:
+            self.btnDownload.setEnabled(False)
 
     def youtube_dl_version_check(self):
         new_version = self.yt.check_version()
         if new_version and new_version != 'Unable to get version':
             res = QMessageBox.warning(self,
-                                     "youtube-dl version check",
-                                     """A new version of youtube-dl is available, would you like to download it now?""",
-                                     QMessageBox.Yes | QMessageBox.No,
-                                     WindowModility=True)
+                                      "youtube-dl version check",
+                                      """A new version of youtube-dl is available, would you like to download it now?""",
+                                      QMessageBox.Yes | QMessageBox.No,
+                                      WindowModility=True)
             if res == QMessageBox.StandardButton.Yes:
-                self.yt.download_file(new_version)
+                self.yt.download_file(new_version, True)
         elif new_version == 'Unable to get version':
             QMessageBox.warning(self,
                                 "youtube-dl version check",
@@ -128,6 +167,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                 QMessageBox.Ok,
                                 WindowModility=True)
 
+    def ffmpeg_check(self):
+        ffmpeg_url = self.yt.check_ffmpeg()
+        if ffmpeg_url:
+            res = QMessageBox.warning(self,
+                                      "ffmpeg check",
+                                      """ffmpeg is missing, without this you will not be able to download mp3 files.
+Would you like to download it now?""",
+                                      QMessageBox.Yes | QMessageBox.No,
+                                      WindowModility=True)
+            if res == QMessageBox.StandardButton.Yes:
+                self.yt.download_file(ffmpeg_url)
+
+    def ffprobe_check(self):
+        ffprobe_url = self.yt.check_ffprobe()
+        if ffprobe_url:
+            res = QMessageBox.warning(self,
+                                      "ffprobe check",
+                                      """ffprobe is missing, without this you will not be able to download mp3 files.
+Would you like to download it now?""",
+                                      QMessageBox.Yes | QMessageBox.No,
+                                      WindowModility=True)
+            if res == QMessageBox.StandardButton.Yes:
+                self.yt.download_file(ffprobe_url)
 
     def source_rb_changed(self):
         if self.rbUrl.isChecked():
@@ -141,6 +203,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         else:
             self.txtbxSearch.setEnabled(False)
             self.txtbxUrl.setEnabled(False)
+        self.check_dl_ready()
 
     def youtube_search(self):
         self.statusbar.showMessage('Searching Youtube...')
@@ -158,6 +221,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                                  """Your search term returned no results""",
                                  QMessageBox.Ok)
         self.statusbar.clearMessage()
+        self.check_dl_ready()
 
     def output_folder_chooser(self):
         dialog = QFileDialog()
@@ -166,6 +230,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         dialog.exec_()
         dirname = os.path.abspath(dialog.directory().absolutePath())
         self.lblOutputDir.setText(dirname)
+        self.check_dl_ready()
 
     def dataReady(self):
         cursor = self.txtOutputText.textCursor()
@@ -190,33 +255,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.btnDownload.setEnabled(True)
             return
 
-        
-
         self.statusbar.showMessage('Downloading file...')
         self.start_download()
 
     def start_download(self):
-        outputdir = str(self.lblOutputDir.text()).replace('\\','/')
+        outputdir = str(self.lblOutputDir.text()).replace('\\', '/')
         if self.rbDlVideo.isChecked():
             itemtype = 'video'
             self.yt.video_download(self.txtbxUrl.text(), outputdir,)
-            #'''youtube-dl --proxy "" --output "{0}/%(title)s" "{1}"'''.format(outputdir, video_link)
-            #params = ['--proxy \"\"','--output \"{0}/%(title)s\"'.format(outputdir),'\"{0}\"'.format(self.txtbxUrl.text())]
+            # '''youtube-dl --proxy "" --output "{0}/%(title)s" "{1}"'''.format(outputdir, video_link)
+            # params = ['--proxy \"\"', '--output \"{0}/%(title)s\"'.format(outputdir), '\"{0}\"'.format(self.txtbxUrl.text())]
         elif self.rbDlAudio.isChecked():
             itemtype = 'audio'
             self.yt.audio_download(self.txtbxUrl.text(), outputdir,)
-        #self.process.execute(os.path.join(os.getcwd(),'youtube-dl.exe'), params)
+        # self.process.execute(os.path.join(os.getcwd(), 'youtube-dl.exe'), params)
         self.statusbar.clearMessage()
         QMessageBox.information(self,
                                 "YouTube Download Complete",
                                 """Your {0} has been successfully downloaded""".format(itemtype),
                                 QMessageBox.Ok)
         self.btnDownload.setEnabled(True)
-        
+
 
 def main():
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon('ytd.ico'))
+    if getattr(sys, 'frozen', False):
+        # frozen
+        appdir = os.path.dirname(sys.executable)
+    else:
+        # unfrozen
+        appdir = os.path.dirname(os.path.realpath(__file__))
+    app.setWindowIcon(QIcon(os.path.join(appdir, 'ytd.ico')))
     frame = MainWindow()
     frame.show()
     app.exec_()
